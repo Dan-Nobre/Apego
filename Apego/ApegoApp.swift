a8017344b84c633d66697bb9f05172f3f41e9f37
Dan-Nobre <danielaraujonobre14@gmail.com>
2024-06-17 13:51:09 -0300

    Initial Commit

---
+++ Apego/ApegoApp.swift
@@ -0,0 +1,32 @@
+//
+//  ApegoApp.swift
+//  Apego
+//
+//  Created by Daniel Nobre on 17/06/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ApegoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
