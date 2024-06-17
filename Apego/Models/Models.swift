88614ba6e014ce0ee5571fd1cb43ce21370a7a6b
Dan-Nobre <danielaraujonobre14@gmail.com>
2024-06-17 14:07:31 -0300

    Criração de Arquitetura MVVM

---
+++ Apego/Models/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Apego
+//
+//  Created by Daniel Nobre on 17/06/24.
+//
+
+import Foundation
---
+++ Apego/Utils/Util.swift
@@ -0,0 +1,8 @@
+//
+//  Util.swift
+//  Apego
+//
+//  Created by Daniel Nobre on 17/06/24.
+//
+
+import Foundation
