c7f0be71ff02549e38ad7b731501f64ca13fc0fd
taissavitoria <taissavrp@gmail.com>
2024-06-28 11:40:47 -0300

    add slashcreen nova

---
+++ SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  Apego
+//
+//  Created by Taissa Vitoria Rodrigues de Paula on 28/06/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
